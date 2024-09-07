223d216af4e2e63cb11f3adc62cccfc2dfb59dbd
Ayan Bishnoi <ayan209e@gmail.com>
2024-09-07 12:03:59 +0530

    feat: navbar added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import '@/assets/styles/globals.css';
+import Navbar from '@/components/Navbar';
 
 export const metadata = {
     title: 'Property Pulse | Find The Perfect Rental',
@@ -11,6 +12,7 @@ const MainLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
+        <Navbar />
         <div>{children}</div>
       </body>
     </html>
